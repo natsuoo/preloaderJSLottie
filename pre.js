b780969bfdac422e240bc37e1fafeb5ae72bfad9
natsuoo <oinatsuo@gmail.com>
2024-06-14 20:48:17 -0300

    Update pre.js

--- pre.js
+++ pre.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://cdn.jsdelivr.net/gh/jasminder/flutter_preloader/preload.svg';
+svgImage.src = 'https://natsuoo.github.io/preloaderJSLottie/Ou1ProZAIe.svg';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
